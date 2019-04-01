f9e2502269115fcf44f49bc00813c30ee6e1ca9d
Francisco Diosdado <drz.y2k@gmail.com>
2019-03-31 20:26:26 -0600

    sensor 2

--- Examen_U4/Examen_U4.ino
+++ Examen_U4/Examen_U4.ino
@@ -1,4 +1,9 @@
 #include <LiquidCrystal.h>
+#include "SR04.h"
+#define TRIG_PIN 13
+#define ECHO_PIN 10
+SR04 sr04 = SR04(ECHO_PIN, TRIG_PIN);
+long a;
 
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
@@ -11,6 +16,16 @@ int estadoboton1 = 0;
 int estadoboton2 = 0;
 int estadoboton3 = 0;
 int estadoboton4 = 0;
+
+int trig = 13;
+int echo = 10;
+int duracion;
+int distancia;
+
+int LDRpin = A0;
+int LDRvalor = 0;
+int LDRled = 13;
+float LDRvoltaje = 0.0;
 
 
 //b1=8 b2=9
@@ -34,6 +49,9 @@ void setup() {
   pinMode(7, INPUT);
   digitalWrite(7, HIGH);
 
+  pinMode(LDRpin, INPUT);
+  pinMode(LDRled, OUTPUT);
+
 }
 
 void loop() {
@@ -41,6 +59,19 @@ void loop() {
 
     lcd.setCursor( 2, 0 );
     lcd.print( "SENSOR 1" );
+    do {
+      lcd.clear();
+      lcd.setCursor( 2, 0 );
+      lcd.print( "SENSOR 1" );
+      a = sr04.Distance();
+      Serial.print(a);
+      Serial.println("cm");
+      lcd.setCursor( 2, 1 );
+      lcd.print( a );
+      lcd.print("cm");
+      delay(1000);
+    } while (digitalRead( 9 ) == HIGH && digitalRead( 6 )  == HIGH && digitalRead( 7 )  == HIGH);
+
   }
   btnS1 = digitalRead( 8 );
 
@@ -48,6 +79,18 @@ void loop() {
 
     lcd.setCursor( 2, 0 );
     lcd.print( "SENSOR 2" );
+    do {
+      lcd.clear();
+      lcd.setCursor( 2, 0 );
+      lcd.print( "SENSOR 1" );
+
+      LDRvalor = analogRead(LDRpin);
+      LDRvoltaje = LDRvalor * (5.0 / 1023.0);
+      lcd.setCursor( 2, 1 );
+      lcd.print("V: ");
+      lcd.print(LDRvoltaje);
+      delay(500);
+    } while (btnS2 == HIGH && digitalRead( 8 ) == HIGH && digitalRead( 6 )  == HIGH && digitalRead( 7 )  == HIGH);
   }
   btnS2 = digitalRead( 9 );
 
