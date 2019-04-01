32a3cf515a8c32887559e640960fb4a97c7d6bba
Francisco Diosdado <drz.y2k@gmail.com>
2019-03-31 22:00:04 -0600

    todos los sensores

--- Examen_U4/Examen_U4.ino
+++ Examen_U4/Examen_U4.ino
@@ -5,6 +5,12 @@
 SR04 sr04 = SR04(ECHO_PIN, TRIG_PIN);
 long a;
 
+#include <dht_nonblocking.h>
+#define DHT_SENSOR_TYPE DHT_TYPE_11
+
+static const int DHT_SENSOR_PIN = 18;
+DHT_nonblocking dht_sensor( DHT_SENSOR_PIN, DHT_SENSOR_TYPE );
+
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 int btnS1 = 0;
@@ -26,6 +32,12 @@ int LDRpin = A0;
 int LDRvalor = 0;
 int LDRled = 13;
 float LDRvoltaje = 0.0;
+
+int ThermistorPin = 16;
+int Vo;
+float R1 = 10000;
+float logR2, R2, T, Tc, Tf;
+float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
 
 
 //b1=8 b2=9
@@ -52,9 +64,42 @@ void setup() {
   pinMode(LDRpin, INPUT);
   pinMode(LDRled, OUTPUT);
 
+  pinMode(16, INPUT);
+
 }
 
+static bool measure_environment( float *temperature, float *humidity )
+{
+  static unsigned long measurement_timestamp = millis( );
+
+  /* Measure once every four seconds. */
+  if ( millis( ) - measurement_timestamp > 3000ul )
+  {
+    if ( dht_sensor.measure( temperature, humidity ) == true )
+    {
+      measurement_timestamp = millis( );
+      return ( true );
+    }
+  }
+
+  return ( false );
+}
+
+
 void loop() {
+
+  float temperature;
+  float humidity;
+
+  if ( measure_environment( &temperature, &humidity ) == true )
+  {
+    Serial.print( "T = " );
+    Serial.print( temperature, 1 );
+    Serial.print( " deg. C, H = " );
+    Serial.print( humidity, 1 );
+    Serial.println( "%" );
+  }
+
   if ( digitalRead( 8 ) == LOW && btnS1 == HIGH && digitalRead( 9 ) == HIGH && digitalRead( 6 )  == HIGH && digitalRead( 7 )  == HIGH) {
 
     lcd.setCursor( 2, 0 );
@@ -82,7 +127,7 @@ void loop() {
     do {
       lcd.clear();
       lcd.setCursor( 2, 0 );
-      lcd.print( "SENSOR 1" );
+      lcd.print( "SENSOR 2" );
 
       LDRvalor = analogRead(LDRpin);
       LDRvoltaje = LDRvalor * (5.0 / 1023.0);
@@ -90,14 +135,29 @@ void loop() {
       lcd.print("V: ");
       lcd.print(LDRvoltaje);
       delay(500);
-    } while (btnS2 == HIGH && digitalRead( 8 ) == HIGH && digitalRead( 6 )  == HIGH && digitalRead( 7 )  == HIGH);
+    } while (digitalRead( 8 ) == HIGH && digitalRead( 6 )  == HIGH && digitalRead( 7 )  == HIGH);
   }
   btnS2 = digitalRead( 9 );
+
+  //SENSOR 3
 
   if ( digitalRead( 6 ) == LOW && btnS3 == HIGH && digitalRead( 8 ) == HIGH && digitalRead( 9 )  == HIGH && digitalRead( 7 )  == HIGH) {
 
     lcd.setCursor( 2, 0 );
     lcd.print( "SENSOR 3" );
+    do {
+      lcd.clear();
+      lcd.setCursor( 2, 0 );
+      lcd.print( "SENSOR 3" );
+
+      int digitVal = analogRead(15);
+
+      lcd.setCursor( 2, 1 );
+      lcd.print(digitVal);
+
+
+      delay(500);
+    } while (digitalRead( 8 ) == HIGH && digitalRead( 9 )  == HIGH && digitalRead( 7 )  == HIGH);
   }
   btnS3 = digitalRead( 6 );
 
@@ -105,6 +165,24 @@ void loop() {
 
     lcd.setCursor( 2, 0 );
     lcd.print( "SENSOR 4" );
+    do {
+      lcd.clear();
+      lcd.setCursor( 2, 0 );
+      lcd.print( "SENSOR 4" );
+
+      Vo = analogRead(16);
+      R2 = R1 * (1023.0 / (float)Vo - 1.0);
+      logR2 = log(R2);
+      T = (1.0 / (c1 + c2 * logR2 + c3 * logR2 * logR2 * logR2));
+      Tc = T - 273.15;
+
+      lcd.setCursor( 2, 1 );
+      lcd.print("T: ");
+      lcd.print(Tc);
+      lcd.println(" C");
+
+      delay(500);
+    } while (digitalRead( 8 ) == HIGH && digitalRead( 9 )  == HIGH && digitalRead( 6 )  == HIGH);
   }
   btnS4 = digitalRead( 7 );
 
